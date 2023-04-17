0932851fcc123899faad1c1881b3714093c2c755
Dao Le Nhu <62839144+timothy68@users.noreply.github.com>
2023-04-17 16:31:25 +0700

    Update content-single.php
    Update function

--- content-single.php
+++ content-single.php
@@ -1,33 +1,41 @@
-<div class="related-entries">
+<div class="related-posts">
   <?php
-    $post_tags = wp_get_post_tags( $post->ID );
-    if ( $post_tags ) {
-      $tag_ids = array();
-      foreach( $post_tags as $tag ) {
-        $tag_ids[] = $tag->term_id;
-      }
-      $args = array(
-        'tag__in' => $tag_ids,
-        'post__not_in' => array( $post->ID ),
-        'posts_per_page' => 5,
-        'ignore_sticky_posts' => true
-      );
-      $related_posts_query = new WP_Query( $args );
-      if ( $related_posts_query->have_posts() ) {
+    $related_posts = get_related_posts();
+
+    if ( $related_posts->have_posts() ) :
+      echo '<h3>Related Posts</h3>';
+      echo '<ul>';
+      while ( $related_posts->have_posts() ) : $related_posts->the_post();
   ?>
-  <div class="block">
-    <h3>Related Posts</h3>
-    <ul>
-      <?php while ( $related_posts_query->have_posts() ) {
-        $related_posts_query->the_post();
-      ?>
-      <li><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></li>
-      <?php } ?>
-    </ul>
-  </div>
+        <li>
+          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        </li>
   <?php
-      }
-      wp_reset_postdata();
-    }
+      endwhile;
+      echo '</ul>';
+    endif;
+
+    wp_reset_postdata();
   ?>
 </div>
+
+<?php
+function get_related_posts() {
+  global $post;
+
+  $tags = wp_get_post_tags( $post->ID );
+  $tag_ids = array();
+  foreach( $tags as $tag ) {
+    $tag_ids[] = $tag->term_id;
+  }
+
+  $related_posts_query = new WP_Query( array(
+    'tag__in' => $tag_ids,
+    'post__not_in' => array( $post->ID ),
+    'posts_per_page' => 5,
+    'ignore_sticky_posts' => true
+  ) );
+
+  return $related_posts_query;
+}
+?>
